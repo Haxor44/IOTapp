4263417e0cdb365826a8404d7afdeeea3d8db29a
Haxor44 <evolmalek04@gmail.com>
2022-11-29 11:34:33 +0300

    some new additions

--- iot.php
+++ iot.php
@@ -4,6 +4,12 @@
 	echo "Soil moisture level is:".$data;
 	$soil="Soil moisture level is:".$data;
 	$files = file_put_contents('soil.txt', $soil);
+	if ($files !=false) {
+		echo "Success!!!";
+	}
+	else{
+		echo "Did not upload";
+	}
 
  ?>
 
