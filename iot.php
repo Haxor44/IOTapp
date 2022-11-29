e3f7ac88c7f3b0ce9c5cebb95e1c7dbcf110103c
Haxor44 <evolmalek04@gmail.com>
2022-11-29 11:28:51 +0300

    some new additions

--- iot.php
+++ iot.php
@@ -7,5 +7,4 @@
 
  ?>
 
- //GET /iot.php HTTP/1.1\r\nHost: soilapp.herokuapp.com\r\n\r\n
- //POST /iot.php HTTP/1.1\r\nHost: esp8266.allaboutee.com\r\nContent-Type: application/x-www-form-urlencoded\r\nContent-Length: 20\r\n\r\napples=12&oranges=45\r\n\r\n
+ 
