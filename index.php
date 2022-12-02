f751938b5c993cc5cf695eb7e4eb70cb3dab0923
Haxor44 <50330948+Haxor44@users.noreply.github.com>
2022-12-02 12:57:36 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,43 +1,33 @@
-
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="style.css">
     <title>SoilApp</title>
 </head>
-
 <body>
-    <header>
-        <div class="nav-wrapper">
-            <div class="logo-container">
-                <em>SoilApp</em>
+    <nav class="navbar">
+        <div class="navbar-container container">
+            <input type="checkbox" name="" id="">
+            <div class="hamburger-lines">
+                <span class="line line1"></span>
+                <span class="line line2"></span>
+                <span class="line line3"></span>
             </div>
-            <nav>
-                <input class="hidden" type="checkbox" id="menuToggle">
-                <label class="menu-btn" for="menuToggle">
-                    <div class="menu"></div>
-                    <div class="menu"></div>
-                    <div class="menu"></div>
-                </label>
-                <div class="nav-container">
-                    <ul class="nav-tabs">
-                        <li><a href="#">Home</a></li>
-                	<li><a href="login.html">Logout</a></li>
-                    </ul>
-                </div>
-            </nav>
-        </div>
-    </header>
-    <div class="center"> 
-        <h1>Soil moisture is: <?php 
+            <ul class="menu-items">
+                <li><a href="#">Home</a></li>
+                <li><a href="login.html">Login</a></li>
+                <li><a href="registration.html">registration</a></li>
+                <li><a href="#"> <h1> <?php 
     
     $data = $_POST['data'];
     echo "Soil moisture level is:".$data;
  
 
- ?></h1>
-    </div> 
+ ?</a></li>
+            </ul>
+            <h1 class="logo">SoilApp</h1>
+        </div>
+    </nav>
 </body>
 </html>
