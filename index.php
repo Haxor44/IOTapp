fe084b4d7c4a0ac20b794056283e5427bcf6f894
Haxor44 <50330948+Haxor44@users.noreply.github.com>
2022-12-02 12:28:34 +0300

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
   if (isset($_GET['logout'])) {
     session_destroy();
     unset($_SESSION['loggedin']);
-    header("location: login.php");
+    header("location: login.html");
   }
 ?>
 <html lang="en">
